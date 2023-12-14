f979b7cb1119706d5068db1c47ddda4e51efc719
Nabin Hamal <nabinhamal2@gmail.com>
2023-12-14 10:25:27 +0545

    login api connected

---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, {  useLayoutEffect,useContext } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserType } from '../UserContext';
+
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    const {userId,setUserId} = useContext(UserType)
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          headerTitle: "",
+          headerLeft: () => (
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Swift Chat</Text>
+          ),
+          headerRight: () => (
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+              <Ionicons  name="chatbox-ellipses-outline" size={24} color="black" />
+              <MaterialIcons
+                onPress={() => navigation.navigate("Friends")}
+                name="people-outline"
+                size={24}
+                color="black"
+              />
+            </View>
+          ),
+        });
+      }, []);
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,30 +1,127 @@
-import { KeyboardAvoidingView, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState ,useEffect} from "react";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginScreen = () => {
-    const [email,setEmail]= useState('')
-    const [password,setPassword]= useState('')
-    const navigation= useNavigation(); 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem("authToken");
+
+        if (token) {
+          navigation.replace("Home");
+        } else {
+          // token not found , show the login screen itself
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+  const handleLogin = () => {
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://192.168.1.66:8000/login", user)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        AsyncStorage.setItem("authToken", token);
+
+        navigation.replace("Home");
+      })
+      .catch((error) => {
+        Alert.alert("Login Error", "Invalid email or password");
+        console.log("Login Error", error);
+      });
+  };
   return (
-    <View style={{flex:1, backgroundColor:'white',padding:10 , alignItems:'center'}}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
       <KeyboardAvoidingView>
-        <View style={{marginTop:100,justifyContent:'center',alignItems:'center'}}>
-            <Text style={{color:'#4A55A2',fontSize: 17,fontWeight:'600'}} >
-                SIGN IN
+        <View
+          style={{
+            marginTop: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}>
+            Sign In
+          </Text>
+
+          <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+            Sign In to Your Account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 50 }}>
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Email
             </Text>
-                <Text style={{fontSize: 17,fontWeight:'600',marginTop:15}} >Sign In To Your Account </Text>
-        </View>
-        <View style={{marginTop:50}} >
-            <Text>Email</Text>
-            <TextInput value={email} onChange={(text) => setEmail(text)} placeholderTextColor={'black'} placeholder='Enter Your Email' style={{fontSize:email ? 18: 18 ,borderBottomColor:'gray',borderBottomWidth:1,marginVertical:10,width:300}}/>
-        </View>
-        <View style={{marginTop:15}} >
-            <Text>Password</Text>
-            <TextInput value={password} onChange={(text) => setPassword(text)} secureTextEntry={true} placeholderTextColor={'black'} placeholder='Enter Your Password' style={{fontSize:email ? 18: 18 ,borderBottomColor:'gray',borderBottomWidth:1,marginVertical:10,width:300}}/>
-        </View>
-        <Pressable
-           
+
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: email ? 18 : 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="enter Your Email"
+            />
+          </View>
+
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Password
+            </Text>
+
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                fontSize: email ? 18 : 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Passowrd"
+            />
+          </View>
+
+          <Pressable
+            onPress={handleLogin}
             style={{
               width: 200,
               backgroundColor: "#4A55A2",
@@ -46,20 +143,21 @@ const LoginScreen = () => {
               Login
             </Text>
           </Pressable>
+
           <Pressable
             onPress={() => navigation.navigate("Register")}
             style={{ marginTop: 15 }}
           >
             <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
-              Dont't have an account? SIGN UP
+              Dont't have an account? Sign Up
             </Text>
           </Pressable>
-
-        </KeyboardAvoidingView>
+        </View>
+      </KeyboardAvoidingView>
     </View>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
